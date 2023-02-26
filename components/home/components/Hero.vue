b88933142a668a1e4dd0576c5dcf823b5a30c1d7
IDerr <ibrahim@derraz.fr>
2023-02-26 23:58:17 +0100

    :fire: begin index work

---
+++ components/home/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="h-[90vh] w-full relative">
+        <div class="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+            <h1 class="text-2xl-semi mb-4 drop-shadow-md shadow-black">
+                Summer styles are finally here
+            </h1>
+            <p class="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+                This year, our new summer collection will shelter you from the harsh
+                elements of a world that doesn&apos;t care if you live or die.
+            </p>
+            <NuxtLink to="/store">Explore products</NuxtLink >
+        </div>
+        <NuxtImg
+            src="/hero.jpg"
+            fit="cover"
+            loading="eager"
+            quality=90
+            alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
+            class="absolute inset-0"
+            draggable="false"
+        />
+    </div>
+</template>
+<script setup ts>
+</script>
