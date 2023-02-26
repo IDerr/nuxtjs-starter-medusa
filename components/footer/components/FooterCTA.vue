65069e84db996bfb6119b65b701e49394f31e5a1
IDerr <ibrahim@derraz.fr>
2023-02-26 23:58:07 +0100

    :star: add footer types

---
+++ components/footer/components/FooterCTA.vue
@@ -0,0 +1,22 @@
+<template>
+    <div className="bg-amber-100 w-full">
+        <div className="content-container flex flex-col-reverse gap-y-8 small:flex-row small:items-center justify-between py-16 relative">
+            <div>
+                <h3 className="text-2xl-semi">Shop the latest styles</h3>
+                <div className="mt-6">
+                <UnderlineLink href="/store">Explore products</UnderlineLink>
+                </div>
+            </div>
+
+            <div className="relative w-full aspect-square small:w-[35%] small:aspect-[28/36]">
+                <NuxtImg
+                src="/cta_three.jpg"
+                alt=""
+                layout="fill"
+                fit="cover"
+                class="absolute inset-0"
+                />
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/footer/components/FooterNav.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="content-container flex flex-col gap-y-8 pt-16 pb-8">
+      <div class="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between">
+        <div>
+          <NuxtLink href="/">
+            <a class="text-xl-semi uppercase">Acme</a>
+          </NuxtLink>
+        </div>
+        <div class="text-small-regular grid grid-cols-2 gap-x-16">
+          <div class="flex flex-col gap-y-2">
+            <span class="text-base-semi">Collections</span>
+            <ul class="grid grid-cols-1 gap-y-2" :class="[
+                (collections?.length || 0) > 4 ? 'grid-cols-2' : ''
+          ]">
+                <li v-for="item in collections" key={c.id}>
+                  <NuxtLink :to="'/collections/'+item.id">
+                    <a>{item.title}</a>
+                  </NuxtLink>
+                </li>
+            </ul>
+          </div>
+          <div class="flex flex-col gap-y-2">
+            <span class="text-base-semi">Medusa</span>
+            <ul class="grid grid-cols-1 gap-y-2">
+              <li>
+                <a
+                  href="https://github.com/medusajs"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://docs.medusajs.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/medusajs/nextjs-starter-medusa"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Source code
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col-reverse gap-y-4 justify-center xsmall:items-center xsmall:flex-row xsmall:items-end xsmall:justify-between">
+        <span class="text-xsmall-regular text-gray-500">
+          © Copyright 2022 ACME
+        </span>
+        <div class="min-w-[316px] flex xsmall:justify-end">
+          <CountrySelect />
+        </div>
+      </div>
+    </div>
+</template>
+<script setup ts>
+collections = []
+</script>
